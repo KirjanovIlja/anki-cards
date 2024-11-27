bade97db2e6985e8eb42890f5d17bee3963e145e
Ilja Kirjanov <ilja.kirjanov@unit8.co>
2024-11-27 22:13:14 +0100

    add pre-commit

--- README.md
+++ README.md
@@ -5,12 +5,10 @@ The following functions are implemented:
 
 1. Generation of dataset of words on any topic, like something specific e.g "food", or a type of words - like "verbs for B1 level", or any other - like "mood description", "basic phrases for communication at an airport".
 
-2. For the generated words or user-selected words, translation to any language of choice will be generated. 
+2. For the generated words or user-selected words, translation to any language of choice will be generated.
 
 3. For the pairs of words, simple image will be brought from the internet to enhance assosiciative learning of the language.
 
-4. Anki deck will be generated which later can be imported into Anki app. 
+4. Anki deck will be generated which later can be imported into Anki app.
 
-5. Next step, would be to integrate the system with voice over API, so for each of the phrases user could get an audio. 
-
-
+5. Next step, would be to integrate the system with voice over API, so for each of the phrases user could get an audio.
--- anki-cards/backend/deck.py
+++ anki-cards/backend/deck.py
@@ -2,16 +2,18 @@ from genanki import Model, Deck, Note
 import random
 import pandas as pd
 
+
 class AnkiDeck:
 
-    def __init__(self, 
-                 name: str, 
-                 model: Model, 
-                 data: pd.DataFrame,
-                 front: str,
-                 back: str,
-                 image: str = "Image"
-                ) -> None:
+    def __init__(
+        self,
+        name: str,
+        model: Model,
+        data: pd.DataFrame,
+        front: str,
+        back: str,
+        image: str = "Image",
+    ) -> None:
         self.name = name
         self.model = model
         self.data = data
@@ -25,29 +27,27 @@ class AnkiDeck:
     def generate_id(self) -> str:
 
         id = random.randint(1_000_000_000, 2_147_483_647)
-        
-        return f"{name}_{front}_{back}_{id}"
-    
-    def generate_image_tag(self) -> str:
-        
+
+        return f"{self.name}_{self.front}_{self.back}_{id}"
+
+    def generate_image_tag(self, row) -> str:
+
         if pd.notna(row[self.image]):
             return f"<img src='{row[self.image]}'>"
         else:
             return ""
 
-    def create_notes(self) -> None: 
+    def create_notes(self) -> None:
 
         for _, row in self.data.iterrows():
             front = row[self.front]
             back = row[self.back]
-            image = self.generate_image_tag()
-            
-            note = Note(
-                model = self.model,
-                fields=[front, back, image]
-            )
-            
+            image = self.generate_image_tag(row)
+
+            note = Note(model=self.model, fields=[front, back, image])
+
             self.deck.add_note(note)
-            
-        
-        
+
+    def get_notes(self):
+
+        return self.deck.notes
